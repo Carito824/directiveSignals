81d5b02da3dada2e03bd94f06dfc860e906222af
Carolina Leon <sclh2408@gmail.com>
2023-06-01 22:48:34 -0500

    actualizado

--- src/app/signals/pages/propierties-page/propierties-page.component.ts
+++ src/app/signals/pages/propierties-page/propierties-page.component.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
+import { User } from '../../interfases/user-request.interface';
 
 @Component({
   templateUrl: './propierties-page.component.html',
   styleUrls: ['./propierties-page.component.css']
 })
-export class PropiertiesPageComponent {
+export class PropiertiesPageComponent implements OnDestroy, OnInit {
+
+  public  counter = signal(10);
+
+  public user = signal<User>({
+        id: 1,
+        email: "janet.weaver@reqres.in",
+        first_name: "Janet",
+        last_name: "Weaver",
+        avatar: "https://reqres.in/img/faces/2-image.jpg"
+  })
+
+  public fullName = computed(() => `${this.user().first_name} ${this.user().last_name}`)
+
+  public userChangedEfect = effect( () => {
+    console.log(`${ this.user().first_name } - ${this.counter()}`);
+  });
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.counter.update(current  => current + 1)
+
+      //if(this.counter() == 15)
+      //this.userChangedEfect.destroy();
+    },1000)
+
+  }
+
+  ngOnDestroy(): void {
+    // this.userChangedEffect.destroy();
+  }
+  increaseBy(value: number) {
+    this.counter.update(current => current + value);
+  }
+  onFieldUpdated( field: keyof User, value: string){
+
+    this.user.update( current => {
+      return {
+        ...current,
+        [field]: value
+      };
+    });
+
+    //this.user.set({
+      //...this.user(),
+      //[field]: value,
+    //})
+    //this.user.mutate( current => {
+
+      //switch(field) {
+       // case 'email':
+      //    current.email = value;
+      //  break;
+      //  case 'first_name':
+      //    current.first_name = value;
+      //  break;
+      //  case 'last_name':
+      //    current.last_name = value;
+      //  break;
+      //  case 'id':
+      //    current.id = Number(value);
+      //  break;
+
+    //  }
+
+    //})
+  }
 
 }
